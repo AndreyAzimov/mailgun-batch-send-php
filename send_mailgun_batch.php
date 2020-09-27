93d76dca4e6d17b8d2ea6be8322ce62a7d7e4ba6
Andrey Azimov <azimov.andrey@gmail.com>
2020-09-27 21:25:08 +0700

    Update send_mailgun_batch.php

--- send_mailgun_batch.php
+++ send_mailgun_batch.php
@@ -3,7 +3,7 @@
   // How to use:
     // 1. add this file into your PHP project
     // 2. require_once "send_mailgun_batch.php"
-    // 3. change $mailgun_api_url and $mailgun_api_key
+    // 3. change $domain and $mailgun_api_key
     // 4. Call this function send_mailgun_batch($emails_to_send_array, $subject, $message);
     // 5. If success you should see a message "Queued. Thank you."
   // Below is the demo that you can remove:
@@ -31,7 +31,8 @@
 			# doc: https://documentation.mailgun.com/en/latest/user_manual.html#batch-sending
 
 			// todo: change to your
-				$mailgun_api_url = "https://api.mailgun.net/v3/email.example.com/messages";
+				$domain = "email.example.com";
+				$mailgun_api_url = "https://api.mailgun.net/v3/" . $domain ."/messages";
 				$mailgun_api_key = 'api:27fb5dff743534dsfg8asdasdg-gdfgdf342-bfgdgdfeg24';
 			//
 
